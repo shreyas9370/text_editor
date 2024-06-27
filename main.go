23c59f6dfdd19f7605978c8265e523b338685b68
shreyas9370 <shreyasraut123@gmail.com>
2024-06-27 11:28:09 +0530

    Adding Server and database conection

--- main.go
+++ main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text-editor/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect database: %v", err)
+	}
+
+	err = db.AutoMigrate(&models.Text{}, &models.History{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
+	r := gin.Default()
+
+	err = r.Run(":8080")
+	if err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
+}
